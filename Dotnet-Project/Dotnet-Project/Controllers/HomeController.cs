1b4ac939cb8aaa6752c3574300ad031ce1c984c2
maram <maram.benrhouma@insat.ucar.tn>
2025-01-28 16:23:05 +0100

    employeeAndMeetingView

--- Dotnet-Project/Dotnet-Project/Controllers/HomeController.cs
+++ Dotnet-Project/Dotnet-Project/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Dotnet_Project.Controllers
 
         public IActionResult Index()
         {
-            return View("Views/Employees/Index.cshtml");
+            return View();
         }
 
         public IActionResult Privacy()
