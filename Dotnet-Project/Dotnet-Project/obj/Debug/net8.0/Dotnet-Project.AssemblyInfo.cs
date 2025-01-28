16617a34dc2b4382f3008089a13f50c107d7d9f5
Jouini-Nour <nour.jouini.558@gmail.com>
2025-01-28 13:39:26 +0100

    Added Entities and Dependencies

--- Dotnet-Project/Dotnet-Project/obj/Debug/net8.0/Dotnet-Project.AssemblyInfo.cs
+++ Dotnet-Project/Dotnet-Project/obj/Debug/net8.0/Dotnet-Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dotnet-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+022516ae892fb3c046b094de2057f1906e6494ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+968fa48503a4cecf00454ad03ef572880905261f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dotnet-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dotnet-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
