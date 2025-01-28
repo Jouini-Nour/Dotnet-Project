64d994c85b6461e7d4477b15e076fe987432730a
unknown <ghoumamaram@gmail.com>
2025-01-28 11:50:36 +0100

    pure css,html and js: an overview over the views

---
+++ Dotnet-Project/Dotnet-Project/Views/Home/tasks.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dotnet_Project.Views.Home
+{
+    public class tasksModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
